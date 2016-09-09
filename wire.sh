50f94481013654d9d8e6633e521530bb95c6bf0c
Saurabh Sharma <saurabh@konstantinfo.com>
2016-09-09 16:44:27 +0530

    Build and export, getting provision profile name in first argument. Exporting in project folder.

--- wire.sh
+++ wire.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-echo "hello wire!!"
+echo "Wire!!"
+
+#find . -maxdepth 1 -name "*.xcodeproj"
+#find . -maxdepth 1 -name '*.xcodeproj' -o -name '*.xcworkspace'
+
+projectfile=`find . -maxdepth 1 -name "*.xcodeproj" | cut -c3- | awk -F'.' '{print $1}'`
+
+echo "$projectfile";
+
+xcodebuild clean -project "$projectfile".xcodeproj -configuration Release -alltargets
+
+xcodebuild archive -project "$projectfile".xcodeproj -scheme "$projectfile" -archivePath "$projectfile".xcarchive
+
+xcodebuild -exportArchive -archivePath "$projectfile".xcarchive -exportPath "$projectfile" -exportFormat ipa -exportProvisioningProfile "$1"
+
+
+
+exit;
