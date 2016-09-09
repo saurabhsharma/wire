1f4e3bc08c6a94ee9b44c5f5f5f0d98b8681b28b
Saurabh Sharma <saurabhsharma@live.com>
2016-09-09 17:36:16 +0530

    taking export location input from user

--- wire.sh
+++ wire.sh
@@ -9,11 +9,28 @@ projectfile=`find . -maxdepth 1 -name "*.xcodeproj" | cut -c3- | awk -F'.' '{pri
 
 echo "$projectfile";
 
+#echo ~/Desktop/"$projectfile/"
+
+read -p "Enter export location (${HOME}/Desktop/$projectfile/):" exportlocation
+
+  
+if [[ -z "$exportlocation" ]]; then
+   exportlocation=`echo ${HOME}/Desktop/$projectfile/`   
+fi
+
+mkdir -p "$exportlocation"
+
+
+exit;
+
+
 xcodebuild clean -project "$projectfile".xcodeproj -configuration Release -alltargets
 
 xcodebuild archive -project "$projectfile".xcodeproj -scheme "$projectfile" -archivePath "$projectfile".xcarchive
 
-xcodebuild -exportArchive -archivePath "$projectfile".xcarchive -exportPath "$projectfile" -exportFormat ipa -exportProvisioningProfile "$1"
+xcodebuild -exportArchive -archivePath "$projectfile".xcarchive -exportPath "$projectfile" -exportFormat ipa
+
+#xcodebuild -exportArchive -archivePath "$projectfile".xcarchive -exportPath "$projectfile" -exportFormat ipa -exportProvisioningProfile "$1"
 
 
 
