9da340d0cbd643a017682728a12a05252af07458
Saurabh Sharma <saurabhsharma@live.com>
2016-09-09 20:06:58 +0530

    build (ipa) exported on user's given location.

--- wire.sh
+++ wire.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-echo "Wire!!"
+echo "--------------------------------------------------------------"
+echo "------------------------------Wire!!--------------------------"
+echo "--------------------------------------------------------------"
 
 #find . -maxdepth 1 -name "*.xcodeproj"
 #find . -maxdepth 1 -name '*.xcodeproj' -o -name '*.xcworkspace'
 
 projectfile=`find . -maxdepth 1 -name "*.xcodeproj" | cut -c3- | awk -F'.' '{print $1}'`
 
-echo "$projectfile";
+#echo "$projectfile";
 
 #echo ~/Desktop/"$projectfile/"
 
@@ -16,21 +18,32 @@ read -p "Enter export location (${HOME}/Desktop/$projectfile/):" exportlocation
   
 if [[ -z "$exportlocation" ]]; then
    exportlocation=`echo ${HOME}/Desktop/$projectfile/`   
+
+else
+	exportlocation=`echo $exportlocation/`   
+
 fi
 
 mkdir -p "$exportlocation"
 
+#echo "$exportlocation$projectfile"
 
-exit;
+ 
 
 
 xcodebuild clean -project "$projectfile".xcodeproj -configuration Release -alltargets
 
-xcodebuild archive -project "$projectfile".xcodeproj -scheme "$projectfile" -archivePath "$projectfile".xcarchive
+xcodebuild archive -project "$projectfile".xcodeproj -scheme "$projectfile" -archivePath "$exportlocation$projectfile".xcarchive
 
-xcodebuild -exportArchive -archivePath "$projectfile".xcarchive -exportPath "$projectfile" -exportFormat ipa
-
+xcodebuild -exportArchive -archivePath "$exportlocation$projectfile".xcarchive -exportPath "$exportlocation$projectfile" -exportFormat ipa
 #xcodebuild -exportArchive -archivePath "$projectfile".xcarchive -exportPath "$projectfile" -exportFormat ipa -exportProvisioningProfile "$1"
+
+
+echo -e "--------------------------------------------------------------------------------------------"
+echo -e "Export Successful at $exportlocation$projectfile"
+echo -e "--------------------------------------------------------------------------------------------${NC}"
+
+
 
 
 
